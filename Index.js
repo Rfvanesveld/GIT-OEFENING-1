32a21a0926f484f30bb9ba6c481939a57457a856
Richard van Esveld <Rfve@me.com>
2021-06-01 14:02:38 +0200

    Mijn eerste Git

---
+++ Index.js
@@ -0,0 +1,43 @@
+console.log('Hello Winc Academy')
+
+// let name = 'Richard'
+// console.log('Richard')
+
+// let a = 4;
+// let b = 4;node
+// let c = a + b;
+// alert(c);
+
+// let sum = 3 + 4;
+// console.log("sum:", sum);
+// let sumWithStrings = "3" + "4";
+// console.log("sum with strings:", sumWithStrings);
+
+
+
+let a = 4;
+let b = 4;
+let c = a * b;
+console.log(c);
+
+// let a = 16;
+// let b = 4;
+// let c = a / b;
+// alert(c);
+
+// let a = 20;
+// let b = 100;
+// let c = a % b;
+// console.log(c);
+
+// let a = 20;
+// let b = 60;
+// let c = a - b;
+// alert(c);
+
+// let age = 47;
+// let age = 'fourtyseven';
+// console.log(typeof age)
+
+
+
